edd07db13d8e62f447d362663d777d5920f3129f
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-07-22 14:15:43 +0530

    #1 Method Overloading

---
+++ Method Overloading/src/Overload.java
@@ -0,0 +1,8 @@
+public class Overload {
+    public static void main(String[] args){
+        OverloadDemo obj1=new OverloadDemo();
+        obj1.test();
+        obj1.test(12);
+        obj1.test(11,14);
+    }
+}
