f7b9e724ff20d9b5744a8f91593a16b8827ecfd4
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-07-22 14:28:13 +0530

    #1 Method Overloading

--- Overloading/Method Overloading/src/Overload.java
+++
@@ -1,7 +0,0 @@
-public class Overload {
-    public static void main(String[] args){
-        OverloadDemo obj1=new OverloadDemo();
-        obj1.test();
-        obj1.test(12);
-    }
-}
--- Overloading/Method Overloading/src/OverloadDemo.java
+++
@@ -1,8 +0,0 @@
-public class OverloadDemo {
-    void test(){
-        System.out.println("No parameters..");
-    }
-    void test(int a){
-        System.out.println("value of A="+a);
-    }
-}
